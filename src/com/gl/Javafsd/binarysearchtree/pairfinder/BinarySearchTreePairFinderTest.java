df467bff1edad2ffdfeaca5b6ac2c7553cc20fa4
12shikhar <99669077+12shikhar@users.noreply.github.com>
2022-05-02 20:45:37 +0530

    Add files via upload

---
+++ src/com/gl/Javafsd/binarysearchtree/pairfinder/BinarySearchTreePairFinderTest.java
@@ -0,0 +1,50 @@
+package com.gl.Javafsd.binarysearchtree.pairfinder;
+
+import java.util.Scanner;
+public class BinarySearchTreePairFinderTest {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the sum to be checked");
+		
+		int sum = sc.nextInt();
+		
+		test(sum);
+//		
+//		test(120);
+//		test(60);
+//		test(190);
+//		test(500);
+	}
+
+	private static void test(int sum) {
+		
+		BinarySearchTree tree = sample1();
+		
+		BinarySearchTreePairFinder finder 
+			= new BinarySearchTreePairFinder(tree);
+		
+		finder.findPair(sum);
+		
+		System.out.println("*********************");
+	}
+	
+	private static BinarySearchTree sample1() {
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		
+		tree.insert(70);
+		tree.insert(50);
+		tree.insert(90);
+		tree.insert(30);
+		tree.insert(60);
+		tree.insert(80);
+		tree.insert(100);
+		tree.insert(20);
+		tree.insert(40);
+	
+		return tree;
+	}
+}
+
---
+++ src/com/gl/Javafsd/ds4/brackets/BracketsTest.java
@@ -0,0 +1,32 @@
+package com.gl.Javafsd.ds4.brackets;
+
+public class BracketsTest {
+	public static void main(String[] args) {
+		
+		test("{[()]}");
+		
+		test("{}");
+
+		
+		test("M[MW]W");
+		test("M[]W");
+		
+	}
+	
+	private static void test(String brackets) {
+			
+		BalancedBracketsChecker checker 
+			= new BalancedBracketsChecker(brackets);
+		boolean result = checker.check();
+		
+		if (result) {
+			System.out.println("Bracket expression '" 
+					+ brackets + "' is balanced");			
+		}else {
+			System.out.println("Bracket expression '" 
+					+ brackets + "' is NOT balanced");
+		}		
+	}
+}
+
+
